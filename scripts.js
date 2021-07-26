559e0616d13c7e9833f05c9de0dc7cded856dd75
Alex On <shuhia.on@gmail.com>
2021-07-26 16:51:31 +0200

    Fix: responsive navbar

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ const formOne = forms["form-1"];
 const scrollUp = document.getElementById("scroll-up");
 const toggleButton = document.getElementById("toggle-button");
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
